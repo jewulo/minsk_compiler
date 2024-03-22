9101fbf2d3bc57c5d2376577227e6df89d4084c0
Joshua Ewulo <jewulo@gmail.com>
2024-03-22 21:08:20 +0000

    Add project files.

---
+++ minsk_compiler/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// https://www.youtube.com/playlist?list=PLRAdsfhKI4OWNOSfS7EUu5GRAVmze1t2y
+
+namespace minsk_compiler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
